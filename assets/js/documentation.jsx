095e301dd59d4df483e37c17779b004871d18f03
onyoo <roberto9m9@gmail.com>
2019-03-31 19:15:14 -0700

    test add routing

--- assets/js/documentation.jsx
+++ assets/js/documentation.jsx
@@ -8,7 +8,91 @@ class HelloMessage extends React.Component {
   }
 }
 
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: null
+    }
+  }
+
+  render() {
+    return (
+      <button className="square"
+              onClick={() => this.setState({value:"X"})}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
 ReactDOM.render(
-  <HelloMessage name="Roberto" />,
-  document.getElementById('hello-example')
+    <Router>
+      <div>
+        <aside>
+          <Link to={`/`}>Dashboard</Link>
+          <Link to={`/about`}>About</Link>
+        </aside>
+
+        <HelloMessage name="Roberto" />
+
+        <main>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/tick" component={Game} />
+        </main>
+
+        // <Game />
+      </div>
+    </Router>,
+  document.getElementById('root')
 );
